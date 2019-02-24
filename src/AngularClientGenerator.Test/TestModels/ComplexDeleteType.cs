8de81299e4d5355ad847d13b4df93ac12bfaeb20
Küzdi Tamás <kuzditomi@gmail.com>
2019-02-24 16:22:15 +0100

    migrate action config tests

---
+++ src/AngularClientGenerator.Test/TestModels/ComplexDeleteType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AngularClientGenerator.Test.TestModels
+{
+    public class ComplexDeleteType
+    {
+        public string CategoryId { get; set; }
+        public string ProfileName { get; set; }
+
+        public IEnumerable<InnerComplexDeleteType> ComplexType { get; set; }
+    }
+
+    public class InnerComplexDeleteType
+    {
+        public string StringProperty { get; set; }
+    }
+}
